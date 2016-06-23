149a16a4d99e5343fc03a1294ca23b368be6c4f9
Matteo <hook@lubuntu.lan>
2016-06-23 20:08:00 +0200

    documentation and package info update

--- README.rst
+++ README.rst
@@ -1,24 +1,4 @@
+Function to produce a line in a format compatible with the software TeamSystem MULTI.
+
 Test script:
 python -m pyIMPPN.tests.pyimppn_test
-
-Easy two-way conversion between Python dictionaries and fixed-width files.
-The FixedWidth class has been used in production without modification for 
-several years.
-
-This module has also proven useful for "debugging" a fixed-width spec --
-an invalid configuration reports an error that may not be obvious from
-reading the spec document.
-
-
-
-Notes:
-
-#.  A field must have a start_pos and either an end_pos or a length.
-    If both an end_pos and a length are provided, they must not conflict.
-
-#.  A field may not have a default value if it is required.
-
-#.  Supported types are string, integer, and decimal.
-
-#.  Alignment and padding are required.
-
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='FixedWidth',
-    version='0.99.3',
-    description='Two-way fixed-width / Python dict converter.',
-    packages=['fixedwidth'],
-    install_requires=['six'],
+    name='pyIMPPN',
+    version='0.0.1',
+    description='TeamSystem Multi line generator (fixed width)',
+    packages=['pyimppn'],
+    install_requires=['fixedwidth'],
     license='BSD',
-    url='https://github.com/ShawnMilo/fixedwidth',
-    author='Shawn Milochik',
-    author_email='shawn@milochik.com',
+    url='https://github.com/matteopolleschi/pyIMPPN',
+    author='Matteo Polleschi',
+    author_email='nomail@matteopolleschi.com',
     zip_safe=False,
-    keywords='fixed width',
-    test_suite="fixedwidth.tests",
+    keywords='IMPPN TeamSystem',
+    test_suite="pyIMPPN.tests",
 )
