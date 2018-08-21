c52941752d3079238942905397958a59ec462e7f
Mounir LAHSINI <mounirlahsini.ml@gmail.com>
2018-08-21 21:15:21 +0100

    module fix

--- README.rst
+++ README.rst
@@ -1,10 +1,1 @@
-Python module pyIMPPN
-======================
-
-Function to produce a line in a format compatible with the software TeamSystem MULTI.
-
-Test script:
-------------
-
-python -m pyIMPPN.tests.pyimppn_test
- 
+README
--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README')) as f:
+    long_description = f.read()
 
 
 setup(
@@ -15,5 +20,8 @@ setup(
     zip_safe=False,
     keywords='IMPPN TeamSystem',
     test_suite="pyIMPPN.tests",
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
 
